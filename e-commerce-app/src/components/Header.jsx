885a4ff7be65ac6f966a498c350b2c765b8f63e8
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2022-11-30 15:34:23 +0530

    added redux, searching

--- e-commerce-app/src/components/Header.jsx
+++ e-commerce-app/src/components/Header.jsx
@@ -1,29 +1,204 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   AppBar,
   Badge,
   Button,
   IconButton,
+  styled,
   Toolbar,
   Typography,
+  alpha,
+  TextField,
+  Autocomplete,
+  Select,
+  MenuItem,
+  useTheme,
 } from '@mui/material';
 import { Box } from '@mui/system';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCartSharp';
+import { useDispatch, useSelector } from 'react-redux';
+import { getItemsCount } from '../utils';
+import { fetchAllCategories } from '../feature/categories-slice';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Search = styled('section')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  display: 'flex',
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+}));
+
+const StyleAutocomplete = styled(Autocomplete)(({ theme }) => ({
+  color: 'inherit',
+  width: '100%',
+  '& .MuiTextField-root': {
+    paddingRight: `calc(1em + ${theme.spacing(4)})`,
+  },
+  '& .MuiInputBase-input': {
+    color: theme.palette.common.white,
+  },
+  '& .MuiOutlinedInput-notchedOutline': {
+    border: 'none',
+  },
+  '& .MuiSvgIcon-root': {
+    fill: theme.palette.common.white,
+  },
+}));
+
+const SearchIconWrapper = styled('section')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  right: 0,
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+function SearchBar() {
+  const theme = useTheme();
+  const products = useSelector((state) => state.products.value);
+  const categories = useSelector((state) => state.categories?.value);
+  const [seletedCategory, setSelectedCategory] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const searchTerm = searchParams.get('searchTerm');
+  const category = searchParams.get('category');
+
+  useEffect(() => {
+    setSelectedCategory(category ? category : 'all');
+  }, [category]);
+
+  if (!categories.length) {
+    dispatch(fetchAllCategories());
+  }
+
+  function handleCategoryChange(event) {
+    const { value } = event.target;
+    navigate(
+      value === 'all'
+        ? '/'
+        : `/?category=${value}${searchTerm ? '&searchterm=' + searchTerm : ''}`
+    );
+  }
+
+  function handleSearchChange(searchText) {
+    if (searchText) {
+      navigate(
+        seletedCategory === 'all'
+          ? `/?searchterm=${searchText}`
+          : `/?category=${seletedCategory}&searchterm=${searchText}`
+      );
+    } else {
+      navigate(
+        seletedCategory === 'all' ? '/' : `/?category=${seletedCategory}`
+      );
+    }
+  }
+
+  return (
+    <Search>
+      <Select
+        value={seletedCategory}
+        onChange={handleCategoryChange}
+        size="small"
+        sx={{
+          textTransform: 'capitalize',
+          m: 1,
+          '&': {
+            '::before': {
+              border: 'none',
+            },
+          },
+          '::before, &::after': {
+            border: 'none',
+          },
+          '.MuiSelect-standard': {
+            color: 'common.white',
+          },
+          '.MuiSelect-icon': {
+            fill: theme.palette.common.white,
+          },
+        }}
+        variant="standard"
+        labelId="selected-category"
+        label="selected-category-id"
+      >
+        <MenuItem
+          sx={{
+            textTransform: 'capitalize',
+          }}
+          value="all"
+        >
+          all
+        </MenuItem>
+        {categories.map((category) => (
+          <MenuItem
+            sx={{
+              textTransform: 'capitalize',
+            }}
+            key={category}
+            value={category}
+          >
+            {category}
+          </MenuItem>
+        ))}
+      </Select>
+      <StyleAutocomplete
+        freeSolo
+        id="selected-product"
+        value={selectedProduct}
+        onChange={(e, value) => {
+          handleSearchChange(value?.label);
+        }}
+        disablePortal
+        options={Array.from(
+          seletedCategory === 'all'
+            ? products
+            : products.filter((prod) => prod.category === seletedCategory),
+          (prod) => ({
+            id: prod.id,
+            label: prod.title,
+          })
+        )}
+        // sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
+    </Search>
+  );
+}
 
 export default function Header() {
+  const cartItems = useSelector((state) => state.cart?.value);
+  const count = getItemsCount(cartItems);
+
   return (
     <AppBar position="sticky">
       <Toolbar>
-        <Typography variant="h6" color="inherit" sx={{ flexGrow: 1 }}>
+        <Typography variant="h6" color="inherit">
           Ecom
         </Typography>
+        <SearchBar />
         <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
           <IconButton
             size="large"
             aria-label="shows cart items count"
             color="inherit"
           >
-            <Badge badgeContent={1} color="error">
+            <Badge badgeContent={count} color="error">
               <ShoppingCartIcon />
             </Badge>
           </IconButton>
--- e-commerce-app/src/pages/Home.jsx
+++ e-commerce-app/src/pages/Home.jsx
@@ -12,83 +12,119 @@ import {
 } from '@mui/material';
 import { useTheme } from '@emotion/react';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCartSharp';
+import { useDispatch, useSelector } from 'react-redux ';
+import { addToCart } from '../feature/cart-slice';
+import { fetchAllProducts } from '../feature/products-slice';
+import { useSearchParams } from 'react-router-dom';
 
 export default function Home() {
+  const [searchParams] = useSearchParams();
+  const category = searchParams.get('category');
   const theme = useTheme();
-  const [products, setProducts] = useState([]);
+  const state = useSelector((state) => state.products);
+  const { value: products, loading } = state ?? {};
+  const dispatch = useDispatch();
+  const searchTerm = searchParams.get('searchterm');
 
-  async function fetchAllProducts() {
-    const response = await fetch('https://fakestoreapi.com/products');
-    const result = await response.json();
-    setProducts(result);
+  if (!products?.length) {
+    dispatch(fetchAllProducts());
   }
 
-  useEffect(() => {
-    fetchAllProducts();
-  }, []);
+  function addProductToCart(product) {
+    dispatch(addToCart({ product, quantity: 1 }));
+  }
+
+  let filteredProducts =
+    category && category !== 'all'
+      ? products.filter((prod) => prod.category === category)
+      : products;
+
+  filteredProducts = searchTerm
+    ? filteredProducts.filter((prod) =>
+        prod.title.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    : filteredProducts;
 
   return (
     <Container sx={{ py: 8 }} maxWidth="lg">
       <Grid container spacing={4}>
-        {products.map(({ title, id, price, description, rating, image }) => (
-          <Grid item key={id} xs={12} sm={6} md={3}>
-            <Card
-              sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-            >
-              <CardMedia
-                component="img"
+        {filteredProducts?.map(
+          ({ title, id, price, description, rating, image }) => (
+            <Grid item key={id} xs={12} sm={6} md={3}>
+              <Card
                 sx={{
-                  alignSelf: 'center',
-                  width: theme.spacing(30),
-                  height: theme.spacing(30),
-                  objectFit: 'contain',
-                  pt: theme.spacing(),
+                  height: '100%',
+                  display: 'flex',
+                  flexDirection: 'column',
                 }}
-                image={image}
-                alt={title}
-              />
-              <CardContent>
-                <Typography
-                  variant="h5"
-                  component="h2"
-                  gutterBottom
+              >
+                <CardMedia
+                  component="img"
                   sx={{
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                    display: '-webkit-box',
-                    '-webkit-line-clamp': '1',
-                    '-webkit-box-orient': 'vertical',
+                    alignSelf: 'center',
+                    width: theme.spacing(30),
+                    height: theme.spacing(30),
+                    objectFit: 'contain',
+                    pt: theme.spacing(),
                   }}
-                >
-                  {title}
-                </Typography>
-                <Typography
-                  color="text.secondary"
-                  paragraph
-                  sx={{
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                    display: '-webkit-box',
-                    '-webkit-line-clamp': '2',
-                    '-webkit-box-orient': 'vertical',
-                  }}
-                >
-                  {description}
-                </Typography>
-                <Typography fontSize="large" paragraph>
-                  {price}
-                </Typography>
-                <Rating readOnly precision={0.5} value={rating.rate} />
-              </CardContent>
-              <CardActions sx={{ alignSelf: 'center' }}>
-                <Button variant="contained">
-                  <ShoppingCartIcon />
-                  Add to cart
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
+                  image={image}
+                  alt={title}
+                />
+                <CardContent>
+                  <Typography
+                    variant="h5"
+                    component="h2"
+                    gutterBottom
+                    sx={{
+                      overflow: 'hidden',
+                      textOverflow: 'ellipsis',
+                      display: '-webkit-box',
+                      WebkitLineClamp: '1',
+                      WebkitBoxOrient: 'vertical',
+                    }}
+                  >
+                    {title}
+                  </Typography>
+                  <Typography
+                    color="text.secondary"
+                    paragraph
+                    sx={{
+                      overflow: 'hidden',
+                      textOverflow: 'ellipsis',
+                      display: '-webkit-box',
+                      WebkitLineClamp: '2',
+                      WebkitBoxOrient: 'vertical',
+                    }}
+                  >
+                    {description}
+                  </Typography>
+                  <Typography fontSize="large" paragraph>
+                    {price}
+                  </Typography>
+                  <Rating readOnly precision={0.5} value={rating.rate} />
+                </CardContent>
+                <CardActions sx={{ alignSelf: 'center' }}>
+                  <Button
+                    variant="contained"
+                    onClick={() =>
+                      addProductToCart({
+                        title,
+                        id,
+                        price,
+                        description,
+                        rating,
+                        image,
+                      })
+                    }
+                  >
+                    <ShoppingCartIcon />
+                    Add to cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          )
+        )}
       </Grid>
     </Container>
   );
